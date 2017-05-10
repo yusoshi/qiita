526336d6dc92b70ba45b222dd85a25b676c0873b
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-05-10 14:08:45 +0900

    Modify unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ pid "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-listen 80
+listen 3000
 timeout 60
 
 preload_app true
