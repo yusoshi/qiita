5ec025cba13c7667430be9c5729da28f4a6f6f63
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-05-23 21:22:45 +0900

    validation

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,14 @@
 class FeedsController < ApplicationController
+  # ユーザーがログインしていない場合、ログインページにリダイレクト
+  before_action :move_to_login_page
+
   def index
+  end
+
+  private
+  def move_to_login_page
+    unless user_signed_in?
+      redirect_to  new_user_session_path
+    end
   end
 end
