a6d563153ce07f3b29be57cf1fd4cdbbc73d10c1
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-05-23 19:22:03 +0900

    Add columns for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521012431) do
+ActiveRecord::Schema.define(version: 20170522092459) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20170521012431) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name",                                null: false
+    t.string   "user"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
