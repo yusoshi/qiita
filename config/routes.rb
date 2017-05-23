531d877cb21f0e382c28b03c9bf566a34f10e970
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-05-23 21:22:53 +0900

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions:     'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root 'feeds#index'
 end
