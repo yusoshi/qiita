9cd023431deec6c96fb27a6e673b351f40265566
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-05-23 21:18:53 +0900

    Create twitter authentication

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # devise :omniauthable, omniauth_providers: [:twitter]
 
   # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def twitter
+    callback_from :twitter
+  end
 
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
@@ -25,4 +26,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  private
+
+  def callback_from(provider)
+    provider = provider.to_s
+
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      binding.pry
+      session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra')
+      redirect_to new_user_registration_url
+    end
+  end
 end
