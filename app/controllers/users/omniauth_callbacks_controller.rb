2712b442c7bdefd45f69aa1c78304e357e84bb60
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-05-23 21:27:50 +0900

    Remove unnecessary code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -38,7 +38,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
-      binding.pry
       session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_url
     end
